a5056eed4e331888220e87fffed6abff586e0822
Mathieu Frenette <silphid@users.noreply.github.com>
2020-09-18 09:09:11 -0400

    Remove snake case conversion
    Signed-off-by: Mathieu Frenette <silphid@users.noreply.github.com>

--- src/internal/execShell.go
+++ src/internal/execShell.go
@@ -5,8 +5,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"regexp"
-	"strings"
 )
 
 func execShell(context Context, command string) error {
@@ -39,20 +37,9 @@ func getEnvFromValues(values Values) []string {
 	// Then values env vars
 	Log("Environment variables:")
 	for key, value := range values {
-		entry := fmt.Sprintf("%s=%v", toSnakeCase(key), value)
+		entry := fmt.Sprintf("%s=%v", key, value)
 		env = append(env, entry)
 		Log(entry)
 	}
 	return env
-}
-
-var (
-	matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
-	matchAllCap   = regexp.MustCompile("([a-z0-9])([A-Z])")
-)
-
-func toSnakeCase(str string) string {
-	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
-	snake  = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
-	return strings.ToUpper(snake)
 }
