ea406441fae57618f6dff648c3fd0463faba704a
Mathieu Frenette <silphid@users.noreply.github.com>
2020-10-27 19:21:18 -0400

    Add tests for LoadActions()
    Signed-off-by: Mathieu Frenette <silphid@users.noreply.github.com>

--- src/internal/specification/load/load_test.go
+++ src/internal/specification/load/load_test.go
@@ -1,6 +1,7 @@
 package load
 
 import (
+	"github.com/Samasource/jen/internal/specification"
 	"github.com/Samasource/jen/internal/specification/executable"
 	"github.com/Samasource/jen/internal/specification/steps/choice"
 	"github.com/Samasource/jen/internal/specification/steps/do"
@@ -212,6 +213,75 @@ do:
 
 			// Load actual from yaml dom
 			actual, err := loadStep(f.Root.(yaml.Map))
+
+			if tt.error != "" {
+				// Ensure proper error was returned
+				assert.NotNil(t, err)
+				assert.Equal(t, err.Error(), tt.error)
+			} else {
+				// Compare with expected actual
+				assert.Nil(t, err)
+				if diff := deep.Equal(actual, tt.expected); diff != nil {
+					t.Error(diff)
+				}
+			}
+		})
+	}
+}
+func TestLoadAction(t *testing.T) {
+	tests := []struct {
+		name     string
+		buffer   string
+		expected []specification.Action
+		error    string
+	}{
+		{
+			name: "two actions with one step each",
+			buffer: `
+action1:
+  - if: Condition 1
+    input:
+      question: Question 1
+      var: Variable 1
+action2:
+  - input:
+      question: Question 2
+      var: Variable 2`,
+			expected: []specification.Action{
+				{
+					Name: "action1",
+					Steps: []executable.Executable{
+						input.Prompt{
+							If:       "Condition 1",
+							Question: "Question 1",
+							Var:      "Variable 1",
+						},
+					},
+				},
+				{
+					Name: "action2",
+					Steps: []executable.Executable{
+						input.Prompt{
+							Question: "Question 2",
+							Var:      "Variable 2",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Load buffer into yaml dom
+			reader := strings.NewReader(tt.buffer)
+			f := new(yaml.File)
+			var err error
+			f.Root, err = yaml.Parse(reader)
+			assert.Nil(t, err)
+
+			// Load actual from yaml dom
+			actual, err := LoadActions(f.Root.(yaml.Map))
 
 			if tt.error != "" {
 				// Ensure proper error was returned
