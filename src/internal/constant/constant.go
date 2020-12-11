70e7423dd163835b12fe8c78a24bf73949860e28
Mathieu Frenette <silphid@users.noreply.github.com>
2020-12-11 10:31:19 -0500

    Prepare jen for v0.2.0 release
    Signed-off-by: Mathieu Frenette <silphid@users.noreply.github.com>

--- src/internal/constant/constant.go
+++ src/internal/constant/constant.go
@@ -4,6 +4,6 @@ const (
 	SpecFileName     = "spec.yaml"
 	TemplatesDirName = "templates"
 	JenFileName      = "jen.yaml"
-	SpecFileVersion  = "0.0.1"
-	JenFileVersion   = "0.0.1"
+	SpecFileVersion  = "0.2.0"
+	JenFileVersion   = "0.2.0"
 )
