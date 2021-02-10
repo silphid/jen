b232f26755d71fac5a926c74be48265cf1c0f1d0
Mathieu Frenette <silphid@users.noreply.github.com>
2021-02-10 09:23:19 -0500

    Cache projectDir
    Signed-off-by: Mathieu Frenette <silphid@users.noreply.github.com>

--- src/internal/project/project.go
+++ src/internal/project/project.go
@@ -3,29 +3,36 @@ package project
 import (
 	"fmt"
 	"os"
-	"path"
+	"path/filepath"
 
 	"github.com/Samasource/jen/src/internal/constant"
 	"github.com/Samasource/jen/src/internal/helpers"
 )
 
+var projectDir string
+
 // GetProjectDir returns the project's root dir. It finds it by looking for the jen.yaml file
 // in current working dir and then walking up the directory structure until it reaches the
 // volume's root dir. If it doesn't find it, it returns an empty string.
 func GetProjectDir() (string, error) {
+	if projectDir != "" {
+		return projectDir, nil
+	}
+
 	dir, err := os.Getwd()
 	if err != nil {
 		return "", fmt.Errorf("finding project's root dir: %w", err)
 	}
 
 	for {
-		filePath := path.Join(dir, constant.JenFileName)
-		if helpers.PathExists(filePath) {
-			return dir, nil
+		path := filepath.Join(dir, constant.JenFileName)
+		if helpers.PathExists(path) {
+			projectDir = path
+			return projectDir, nil
 		}
 		if dir == "/" {
 			return "", nil
 		}
-		dir = path.Dir(dir)
+		dir = filepath.Dir(dir)
 	}
 }
