8b0b77fb336d27a06957c5ecac7de923f199836d
Mathieu Frenette <silphid@users.noreply.github.com>
2021-03-30 08:02:16 -0400

    Add jen version command
    Signed-off-by: Mathieu Frenette <silphid@users.noreply.github.com>

--- src/main.go
+++ src/main.go
@@ -6,8 +6,10 @@ import (
 	"github.com/Samasource/jen/src/cmd"
 )
 
+var version string
+
 func main() {
-	rootCmd := cmd.NewRoot()
+	rootCmd := cmd.NewRoot(version)
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(-1)
 	}
